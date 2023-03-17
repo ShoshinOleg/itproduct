bc45a12ca862fd18c0c788967f6eb1fa62e8a65d
oleg <pochta@pochta.com>
2023-03-17 15:14:38 +0300

    create data service

---
+++ data/lib/controllers/app_post_controller.dart
@@ -0,0 +1,30 @@
+import 'package:data/utils/app_response.dart';
+import 'package:conduit_core/conduit_core.dart';
+
+
+class AppPostController extends ResourceController {
+  final ManagedContext managedContext;
+
+  AppPostController(this.managedContext);
+
+  @Operation.get()
+  Future<Response> getProfile() async {
+    try {
+      // final id = AppUtils.getIdFromAuthHeader(header);
+      // final user = await managedContext.fetchObjectWithID<User>(id)
+      //   ?..removePropertiesFromBackingMap([
+      //     AppConst.accessToken, 
+      //     AppConst.refreshToken
+      //   ]);
+      return AppResponse.ok(
+        // body: user?.backing.contents,
+        message: "Успешное получение постов"
+      );
+    } catch (error) {
+      return AppResponse.serverError(
+        error,
+        message: "Ошибка получения постов"
+      );
+    }
+  }
+}
